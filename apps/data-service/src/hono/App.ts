9148400017d0f1ff99cf122f8d61393e88effab9
leavemydogalone <niklasport88@gmail.com>
2025-11-13 18:05:30 -0800

    routing works based on countrycode

--- apps/data-service/src/hono/App.ts
+++ apps/data-service/src/hono/App.ts
@@ -1,11 +1,24 @@
 import { getLink } from '@repo/data-ops/queries/links';
 import { Hono } from 'hono';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { getDestinationForCountry } from '@/helpers/route-ops';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
 	const id = c.req.param('id');
-	const linkInfoFromDb = await getLink(id);
+	const linkInfo = await getLink(id);
+	if (!linkInfo) {
+		return c.text('Link not found', 404);
+	}
 
-	return c.json(linkInfoFromDb);
+	const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+	if (!cfHeader.success) {
+		return c.text('Invalid Cloudflare headers', 400);
+	}
+
+	const headers = cfHeader.data;
+	console.log(headers);
+	const destination = getDestinationForCountry(linkInfo, headers.country);
+	return c.redirect(destination);
 });
