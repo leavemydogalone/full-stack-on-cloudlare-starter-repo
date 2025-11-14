68c750cf938ff45ad081257505c8410aee2f7bb6
leavemydogalone <niklasport88@gmail.com>
2025-11-13 17:48:36 -0800

    link info from api

---
+++ apps/data-service/src/hono/App.ts
@@ -0,0 +1,11 @@
+import { getLink } from '@repo/data-ops/queries/links';
+import { Hono } from 'hono';
+
+export const App = new Hono<{ Bindings: Env }>();
+
+App.get('/:id', async (c) => {
+	const id = c.req.param('id');
+	const linkInfoFromDb = await getLink(id);
+
+	return c.json(linkInfoFromDb);
+});
