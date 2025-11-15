4cc21dcd8e44c70335e796bf375a0fbd198cb552
leavemydogalone <niklasport88@gmail.com>
2025-11-15 15:40:34 -0800

    added kv storage

--- apps/data-service/src/hono/App.ts
+++ apps/data-service/src/hono/App.ts
@@ -1,15 +1,15 @@
-import { getLink } from '@repo/data-ops/queries/links';
 import { Hono } from 'hono';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
-import { getDestinationForCountry } from '@/helpers/route-ops';
+import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
 	const id = c.req.param('id');
-	const linkInfo = await getLink(id);
+
+	const linkInfo = await getRoutingDestinations(c.env, id);
 	if (!linkInfo) {
-		return c.text('Link not found', 404);
+		return c.text('Destination not found', 404);
 	}
 
 	const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf);
@@ -18,7 +18,7 @@ App.get('/:id', async (c) => {
 	}
 
 	const headers = cfHeader.data;
-	console.log(headers);
 	const destination = getDestinationForCountry(linkInfo, headers.country);
+
 	return c.redirect(destination);
 });
