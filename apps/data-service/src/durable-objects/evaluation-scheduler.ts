69a8dc16f59cfeaf0d87bc81cdc483979aca2a20
leavemydogalone <niklasport88@gmail.com>
2026-01-08 11:26:17 -0800

    worker alarm

--- apps/data-service/src/durable-objects/evaluation-scheduler.ts
+++ apps/data-service/src/durable-objects/evaluation-scheduler.ts
@@ -1,21 +1,52 @@
 import { DurableObject } from 'cloudflare:workers';
+const { DateTime } = require('luxon');
 
-export class EvaluationScheduler extends DurableObject {
-	count: number = 0;
+interface ClickData {
+	accountId: string;
+	linkId: string;
+	destinationUrl: string;
+	destinationCountryCode: string;
+}
+
+export class EvaluationScheduler extends DurableObject<Env> {
+	clickData: ClickData | undefined;
+
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
 		ctx.blockConcurrencyWhile(async () => {
-			this.count = (await ctx.storage.get('count')) || this.count;
+			this.clickData = await ctx.storage.get<ClickData>('click_data');
 		});
 	}
 
-	async increment() {
-		this.count++;
-		await this.ctx.storage.put('count', this.count);
+	async collectLinkClick(accountId: string, linkId: string, destinationUrl: string, destinationCountryCode: string) {
+		this.clickData = {
+			accountId,
+			linkId,
+			destinationUrl,
+			destinationCountryCode,
+		};
+		await this.ctx.storage.put('click_data', this.clickData);
+
+		const alarm = await this.ctx.storage.getAlarm();
+		if (!alarm) {
+			const oneDay = DateTime.now().plus({ hours: 24 }).valueOf();
+			await this.ctx.storage.setAlarm(oneDay);
+		}
 	}
 
-	// this in memory count will always be up to date, don't need to access storage
-	async getCount() {
-		return this.count;
+	async alarm() {
+		console.log('Evaluation scheduler alarm triggered');
+
+		const clickData = this.clickData;
+
+		if (!clickData) throw new Error('Click data not set');
+
+		await this.env.DESTINATION_EVALUATION_WORKFLOW.create({
+			params: {
+				linkId: clickData.linkId,
+				accountId: clickData.accountId,
+				destinationUrl: clickData.destinationUrl,
+			},
+		});
 	}
 }
